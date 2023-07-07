b92f8e251d8051b0a233223439c4eb9bf265a7ec
Amir Sorayaei <amir.s1377@gmail.com>
2023-07-07 18:57:51 +0330

    Written more tests for ActionButtons component

--- __tests__/components/ActionButtons/index.test.tsx
+++ __tests__/components/ActionButtons/index.test.tsx
@@ -1,22 +1,24 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import ActionButtons from "@/components/ActionButtons";
-import { DialogProps } from "@/components/DialogAlert/DialogAlert";
-import { renderWithProviders } from "@/utils/resources/utils-for-tests";
+import { DialogProps } from "@/components/DialogAlert";
+import { renderWithUtils } from "@/utils/resources/utils-for-tests";
 
 describe("Several related tests to action buttons", () => {
-  it("Should have a Tooltip which has a title named Edit", () => {
-    // Arrange
-    const dialogSettings: DialogProps = {
-      title: "title",
-      message: "message",
-      confirmBtn: "Yes",
-    };
+  // Arrange
+  const modifiedProps: DialogProps = {
+    title: "title",
+    message: "message",
+    confirmBtn: "YES",
+    cancelBtn: "NO",
+  };
 
+  it("Should have two Tooltips which are named Edit and Delete", () => {
     // Act
-    renderWithProviders(
+    renderWithUtils(
       <ActionButtons
-        dialogSettings={dialogSettings}
+        dialogSettings={modifiedProps}
         onEdit={jest.fn()}
         onDelete={jest.fn()}
       />
@@ -27,5 +29,88 @@ describe("Several related tests to action buttons", () => {
     // Assertion
     expect(editElement).toBeInTheDocument();
     expect(deleteElement).toBeInTheDocument();
+  });
+
+  it("Should check the visibility of the dialog", async () => {
+    // Act
+    renderWithUtils(
+      <ActionButtons
+        dialogSettings={modifiedProps}
+        onEdit={jest.fn()}
+        onDelete={jest.fn()}
+      />
+    );
+
+    // Assertion
+    const deleteBtnElement = screen.getByTestId("Delete");
+    await userEvent.click(deleteBtnElement);
+
+    const dialogElement = screen.getByTestId("dialog-alert");
+
+    expect(dialogElement).toBeInTheDocument();
+  });
+
+  it("Should have expected title and message on dialog alert when pressing delete button", async () => {
+    // Act
+    renderWithUtils(
+      <ActionButtons
+        dialogSettings={modifiedProps}
+        onEdit={jest.fn()}
+        onDelete={jest.fn()}
+      />
+    );
+
+    // Assertion
+    const deleteBtnElement = screen.getByTestId("Delete");
+    await userEvent.click(deleteBtnElement);
+
+    const titleElement = screen.getByTestId("dialog-title");
+    const messageElement = screen.getByTestId("dialog-message");
+
+    expect(titleElement).toHaveTextContent(modifiedProps.title);
+    expect(messageElement).toHaveTextContent(modifiedProps.message);
+  });
+
+  it("Should check visibility of cancel button", async () => {
+    // Arrange
+    const props: DialogProps = {
+      title: "title",
+      message: "message",
+      confirmBtn: "YES",
+    };
+
+    // Act
+    renderWithUtils(
+      <ActionButtons
+        dialogSettings={props}
+        onEdit={jest.fn()}
+        onDelete={jest.fn()}
+      />
+    );
+
+    // Assertion
+    const deleteBtnElement = screen.getByTestId("Delete");
+    await userEvent.click(deleteBtnElement);
+
+    const dialogCancelButton = screen.queryByTestId("dialog-cancel-btn");
+    expect(dialogCancelButton).not.toBeInTheDocument();
+  });
+
+  it("Should check the visibility of action buttons", async () => {
+    // Act
+    renderWithUtils(
+      <ActionButtons
+        dialogSettings={modifiedProps}
+        onEdit={jest.fn()}
+        onDelete={jest.fn()}
+      />
+    );
+
+    // Assertion
+    const deleteBtnElement = screen.getByTestId("Delete");
+    await userEvent.click(deleteBtnElement);
+
+    const buttons = screen.getAllByRole("button");
+    expect(buttons.length).toBe(2);
   });
 });
